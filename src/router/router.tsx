40accf5dac48703a56992aa28adb2836fe83cb6b
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-02-15 18:33:11 +0900

    feat: implement login page and authentication check with cookies

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PrivateRoute from "./PrivateRouter";
 
+import { lazy } from "react";
+
+const Login = lazy(() => import("../pages/login"))
+
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         {/* 로그인 안해도 되는 라우팅 */}
+        <Route path="/login" element={<Login />} />
         <Route element={<PrivateRoute />}>
           {/* 로그인 해야되는 라우팅 */}
         </Route>
