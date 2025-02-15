3fbc50598de1b4eb6dab63e3a53036a3d8c9d169
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-02-15 20:29:06 +0900

    feat: update MediaPreviewHeader image source in FandomDetail page

--- src/pages/fandom/FandomDetail.tsx
+++ src/pages/fandom/FandomDetail.tsx
@@ -9,7 +9,7 @@ export default function FandomDetail() {
   const tabs = ["Tab 1", "Tab 2", "Tab 3"];
   return (
     <>
-      <MediaPreviewHeader imageSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgzlaqFaLoi5T625SHM81VVE7LV4T2nGN8UA&s" />
+      <MediaPreviewHeader imageSrc="https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/NCTWish-240316.jpg/800px-NCTWish-240316.jpg" />
       <FandomProfile
         profileImageSrc="https://phinf.wevpstatic.net/MjAyNDA5MjRfMTU0/MDAxNzI3MTY4MzQwODAz.inbxWmasfUxMZ6aK-CtXze5AY7GpGqCmjhBrrFjKMIIg.tRb51nQXrv-IPd9D1sraaYHnadDBh_93mVHX2T90H7Eg.JPEG/0b97f398-b833-4283-a1ba-7acaa3249370.jpeg?type=w670"
         fandomName="NCT 127"
