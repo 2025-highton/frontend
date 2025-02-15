683844025f22bb120a87b0c9c10c8588908913db
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-02-15 19:32:05 +0900

    feat: import global styles and remove unused variables from globals.scss

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Router from './router/router.tsx'
 
+import './styles/globals.scss'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router />
