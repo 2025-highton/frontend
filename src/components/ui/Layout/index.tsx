5f02d17eaa0d069fa3bdb4fdd0c09987814c3fa4
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-02-15 20:05:49 +0900

    feat: add minHeight style to Layout component for improved layout consistency

--- src/components/ui/Layout/index.tsx
+++ src/components/ui/Layout/index.tsx
@@ -4,7 +4,7 @@ interface LayoutProps extends React.HTMLAttributes<HTMLDivElement> {
 
 export default function Layout({ children }: LayoutProps) {
     return (
-        <main style={{ padding: "0 30px" }}>
+        <main style={{ padding: "0 30px", minHeight: "100vh" }}>
             {children}
         </main>
     )
