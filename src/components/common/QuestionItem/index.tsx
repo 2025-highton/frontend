2d65555e6f5574f7f01641af87d5905e1efc795d
mini-aron <aronia.dev@gmail.com>
2025-02-16 00:03:35 +0900

    질문 컴포넌트 추가

--- src/components/common/QuestionItem/index.tsx
+++ src/components/common/QuestionItem/index.tsx
@@ -4,40 +4,62 @@ import { useEffect, useState } from "react";
 import s from "./style.module.scss";
 
 interface Props {
+  idx: number;
   question: string;
   content: string;
   profileId: string;
+  comment: string;
 }
 
-export default function ({ question, content, profileId }: Props) {
-  const [imageFile, setImageFile] = useState<File | null>();
-  const getImageFile = (profileId: string) => {
-    try {
-      const response = client({
-        method: "GET",
-        params: {
-          profileId,
-        },
-      });
-      setImageFile(response.imageFile);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+export default function ({
+  idx,
+  question,
+  content,
+  profileId,
+  comment = "계절이 지나가는 하늘에는 가을로 가득 차 계절이 지나가는 하늘에는 가을로 가득 차계절이 지나가는 하늘에는 가을로 가득 차s",
+}: Props) {
+  // const [imageFile, setImageFile] = useState<File | null>();
+  // const getImageFile = (profileId: string) => {
+  //   try {
+  //     const response = client({
+  //       method: "GET",
+  //       params: {
+  //         profileId,
+  //       },
+  //     });
+  //     setImageFile(response.imageFile);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
 
   useEffect(() => {
-    getImageFile(profileId);
+    // getImageFile(profileId);
   }, []);
 
   return (
-    <Flex>
-      <p>{question}</p>
-      <Flex>
+    <Flex direction="column" className={s.container} gap={8}>
+      <p className={s.ellipsisContainer}>
+        <span>{`Q.${idx}`}</span>
+        {question}
+      </p>
+      <Flex direction="row" gap={10}>
         <div>
-          {imageFile ? <img src={imageFile} /> : <div>대충 mock image</div>}
+          {
+            <img
+              className={s.imageContainer}
+              src={profileId}
+              alt="profile image"
+            />
+          }
         </div>
-        <p>{content}</p>
+        <p className={s.text}>{content}</p>
       </Flex>
+      {comment && <div className={s.line}></div>}
+      <p className={s.commentContainer}>
+        <span>A. </span>
+        {comment}
+      </p>
     </Flex>
   );
 }
--- src/components/fandom/QuestionList/index.tsx
+++ src/components/fandom/QuestionList/index.tsx
@@ -1,5 +1,69 @@
+import { Flex } from "@/components/ui";
 import s from "./style.module.scss";
+import { useEffect, useState } from "react";
+import { client } from "@/api/axios";
+import QuestionItem from "@/components/common/QuestionItem";
 
 export default function () {
-  return <>ddddd</>;
+  const [questionList, setQuestionList] = useState([]);
+
+  const getQuestionList = async () => {
+    await client({
+      method: "GET",
+      params: {},
+    });
+  };
+
+  useEffect(() => {
+    getQuestionList();
+  }, []);
+
+  return (
+    <>
+      <Flex direction="column" gap={10} justify="center">
+        <QuestionItem
+          idx={1}
+          question={"질문하세요질문질문~?"}
+          content={"대답대답대답대답ㅇㄹ늂아ㅠㄱㄹㄷㅇ"}
+          profileId={
+            "https://thumb.mtstarnews.com/06/2024/02/2024022608330769629_2.jpg"
+          }
+        />
+        <QuestionItem
+          idx={1}
+          question={"질문하세요질문질문~?"}
+          content={"대답대답대답대답ㅇㄹ늂아ㅠㄱㄹㄷㅇ"}
+          profileId={
+            "https://thumb.mtstarnews.com/06/2024/02/2024022608330769629_2.jpg"
+          }
+        />
+        <QuestionItem
+          idx={1}
+          question={
+            "질문하세요질문질문~?sssfwfdkdsfdsfwwfljdslsdljdklsjflkdsjlkslsdjfs"
+          }
+          content={"대답대답대답대답ㅇㄹ늂아ㅠㄱㄹㄷㅇ"}
+          profileId={
+            "https://thumb.mtstarnews.com/06/2024/02/2024022608330769629_2.jpg"
+          }
+        />
+        <QuestionItem
+          idx={1}
+          question={"질문하세요질문질문~?"}
+          content={"대답대답대답대답ㅇㄹ늂아ㅠㄱㄹㄷㅇ"}
+          profileId={
+            "https://thumb.mtstarnews.com/06/2024/02/2024022608330769629_2.jpg"
+          }
+        />
+        <QuestionItem
+          idx={1}
+          question={"질문하세요질문질문~?"}
+          content={"대답대답대답대답ㅇㄹ늂아ㅠㄱㄹㄷㅇ"}
+          profileId={
+            "https://thumb.mtstarnews.com/06/2024/02/2024022608330769629_2.jpg"
+          }
+        />
+      </Flex>
+    </>
+  );
 }
