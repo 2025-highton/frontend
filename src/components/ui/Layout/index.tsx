2cde0c033152efaefed1250a29b34c7dbd99549b
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-02-15 22:54:12 +0900

    feat: allow additional props in Layout component for enhanced flexibility

--- src/components/ui/Layout/index.tsx
+++ src/components/ui/Layout/index.tsx
@@ -2,9 +2,9 @@ interface LayoutProps extends React.HTMLAttributes<HTMLDivElement> {
     children: React.ReactNode;
 }
 
-export default function Layout({ children }: LayoutProps) {
+export default function Layout({ children, ...props }: LayoutProps) {
     return (
-        <main style={{ padding: "0 30px", minHeight: "100vh" }}>
+        <main style={{ padding: "0 30px", minHeight: "100vh" }} {...props}>
             {children}
         </main>
     )
